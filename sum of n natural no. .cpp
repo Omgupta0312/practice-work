31f42aede0add37ca03efd861c7effc2c6fb7449
Omgupta0312 <83683230+Omgupta0312@users.noreply.github.com>
2021-05-06 20:19:11 +0530

    Create sum of n natural no. .cpp

---
+++ sum of n natural no. .cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,sum;
+    cout<<"enter no. of natural no.";
+    cin>>n;
+    sum=n*(n+1)/2;
+    cout<<"sum of n natural no. is"<<" "<<sum;
+    
+    return 0;
+    
+}
